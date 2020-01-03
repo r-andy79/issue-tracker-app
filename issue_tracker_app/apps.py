1c37178992308044d31879129ef5df8ba7d2c11a
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2020-01-03 14:06:36 +0000

    Issue tracker app initial commit

---
+++ issue_tracker_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class IssueTrackerAppConfig(AppConfig):
+    name = 'issue_tracker_app'
---
+++ issue_tracker_app/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Ticket(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+    TICKET_TYPES = [
+        ('B', 'bug'),
+        ('F', 'feature')
+    ]
+
+    ticket_type = models.CharField(max_length=1, choices=TICKET_TYPES)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    
+    def __str__(self):
+        return self.title
