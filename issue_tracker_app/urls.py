d2f967a65614c624ca009dc33802c244eb7f7f23
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2020-01-05 18:37:20 +0000

    Added view to display a single ticket

--- issue_tracker_app/urls.py
+++ issue_tracker_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ticket_list, name='ticket_list')
+    path('', views.ticket_list, name='ticket_list'),
+    path('ticket/<int:pk>/', views.ticket_detail, name='ticket_detail')
 ]
--- issue_tracker_app/views.py
+++ issue_tracker_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Ticket
 
@@ -6,3 +6,8 @@ from .models import Ticket
 def ticket_list(request):
     tickets = Ticket.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'issue_tracker_app/ticket_list.html', {'tickets': tickets})
+
+
+def ticket_detail(request, pk):
+    ticket = get_object_or_404(Ticket, pk=pk)
+    return render(request, 'issue_tracker_app/ticket_detail.html', {'ticket': ticket})
