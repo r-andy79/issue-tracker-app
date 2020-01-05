dbad01e1016088c62081c7945071e70c26dcb81e
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2020-01-05 13:52:50 +0000

    Added template showing tickets by type

--- issue_tracker_app/views.py
+++ issue_tracker_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Ticket
 
 # Create your views here.
 def ticket_list(request):
-    return render(request, 'issue_tracker_app/ticket_list.html', {})
+    tickets = Ticket.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'issue_tracker_app/ticket_list.html', {'tickets': tickets})
